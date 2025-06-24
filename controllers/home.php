91e3da5944128f527aab09544a7d08455b745e56
Darlan Rodrigues Schmeller <darlanschimeller@gmail.com>
2025-06-24 15:27:12 -0300

    Add comments, implement query method to Database class, fetch listings

--- controllers/home.php
+++ controllers/home.php
@@ -1,3 +1,8 @@
 <?php
 
+$config = require_once basePath('config/db.php');
+$db = new Database($config);
+
+$listings = $db->query('SELECT * FROM listings LIMIT 6')->fetchAll();
+
 loadView('home');
--- public/index.php
+++ public/index.php
@@ -1,17 +1,17 @@
 <?php
 require_once('../helpers.php');
-
-require_once basePath('Database.php');
-$config = require_once basePath('config/db.php');
-
-$db = new Database($config);
-
 require basePath('Router.php');
+require_once basePath('Database.php');
 
+// Instantiate new Router object
 $router = new Router();
+
+// Get routes
 $routes = require basePath('routes.php');
 
+// Get current URI and HTTP Method
 $uri = $_SERVER['REQUEST_URI'];
 $method = $_SERVER['REQUEST_METHOD'];
 
+// Route the request
 $router->route($uri, $method);
